d774fceee50b0bc5c2cca5af92613bec01aeaeae
sus <edvin.garibovic@gmail.com>
2023-06-16 13:11:13 +0200

    tweaks to enemy pathfinding

--- Scripts/Enemy/enemy.gd
+++ Scripts/Enemy/enemy.gd
@@ -6,7 +6,7 @@ const UP = Vector2(0, -1)
 const GRAVITY = 600.00
 const MAXFALLSPD = 600
 const MAXSPD = 375
-const ACCEL = 400.00
+const ACCEL = 200.00
 const JUMPFORCE = -80
 
 var motion  =  Vector2()
@@ -18,7 +18,7 @@ var time_when_stopped = 0
 var damage = 10
 var threshold = 250
 #var path = PoolVector2Array() setget set_path
-
+var position_old = Vector2(0, 0)
 
 onready var level_name = get_tree().get_current_scene().get_name()
 onready var player = get_tree().get_root().get_node_or_null("./" + level_name + "/Player")
@@ -52,9 +52,8 @@ func _ready():
 # 	move_enemy(delta)      # uhu
 	
 func _physics_process(delta):
-	
 	var finder = find_player()
-	var move_man = applyGravity(delta)
+	var move_man = Vector2(0, 0)
 	
 	if not finder:
 		if finder == null:
@@ -62,88 +61,26 @@ func _physics_process(delta):
 		return
 	
 	move_man += make_path()
+	
+	if position_old == Vector2(0, 0):
+		position_old = move_man
+		
 	if detect_wall():
-		move_man += Vector2(0, -500)
+		move_man += Vector2(0, -100)
+	else: 
+		move_man += applyGravity(delta)
+#		pass
+	if move_man.x > 0 and position_old.x < 0 or move_man.x < 0 and position_old.x > 0 : 
+		self.scale.x = -1
+	position_old = move_man
 	move_man = move_and_slide(move_man, UP)
 
 func make_path():
 	var player_position = player.position
-	var delta_position = Vector2(player_position.x - position.x, player_position.y - position.y).normalized() * 400
-#	print_debug(delta_position)
+#	var delta_position = Vector2(player_position.x - position.x, player_position.y - position.y).normalized() * 400
+	var delta_position = Vector2(player_position.x - position.x, 0).normalized() * 400
 	return delta_position
 	
-	
-#func _physics_process(_delta):
-#	pass
-##	motion += applyGravity(_delta)
-##	move_to_target()
-#
-#func move_along_path(distance:float) -> void: 
-#	var starting_point = position
-#	for i in range(path.size()):
-#		var distance_to_next = starting_point.distance_to(path[0])
-#		if distance < distance_to_next and distance >= 0.0:
-#			position = starting_point.linear_interpolate(path[0], distance/distance_to_next)
-#		elif distance < 0.0: 
-#			position = path[0]
-#			var gravity = Vector2(0, GRAVITY)
-#			if not is_on_floor():
-#				var moment = move_and_slide(gravity, UP)
-#			set_process(false)
-#			break
-#		distance -= distance_to_next
-#		starting_point = path[0]
-#		path.remove(0)
-#
-#func set_path(value: PoolVector2Array) -> void:
-#	path = value
-#	if value.size() == 0:
-#		return
-#	set_process(true)
-
-func applyGravity(_delta):
-	var falling = Vector2(0, 0)
-	falling.y += GRAVITY if not is_on_floor() else 0.20
-	if falling.y > MAXFALLSPD:
-		falling.y = MAXFALLSPD
-	return falling
-
-#func move_to_target():
-#	var direction = global_position.direction_to(navi.get_next_location())
-#	motion = direction * MAXSPD
-#	motion = move_and_slide(motion, UP)
-#
-#func get_target_path(target_pos:Vector2):
-#	navi.set_target_location(target_pos)
-
-
-func detect_wall():
-	return (wall_detect.is_colliding() and wall_detect.get_collider().is_in_group('tilemap'))
-
-#func jump():
-#	motion.y = JUMPFORCE
-#
-#func jump_cut():
-#	if motion.y < -30:
-#		motion.y += 50
-#
-
-#func move_enemy(_delta):
-#
-#	motion.x += -ACCEL if is_moving_left else ACCEL
-#
-#	if abs(motion.x) > MAXSPD:
-#		motion.x = sign(motion.x) * MAXSPD
-#
-#	motion.y += GRAVITY if not is_on_floor() else 0
-#
-#	if motion.y > MAXFALLSPD:
-#		motion.y = MAXFALLSPD
-#
-#	motion = move_and_slide(motion, UP, true)
-#	$AnimatedSprite.play("walk")
-
-# returns true if player is close enough
 func find_player():
 	if player == null:
 		return null
@@ -151,12 +88,21 @@ func find_player():
 	player_location = player.position
 	
 	var distance_to_player = position.distance_to(player_location)
-
 	return not distance_to_player > 700
 #	if distance > 700:
 #		return false
-		
 	
+	
+func applyGravity(_delta):
+	var falling = Vector2(0, 0)
+	falling.y += GRAVITY if not is_on_floor() else 0.20
+	if falling.y > MAXFALLSPD:
+		falling.y = MAXFALLSPD
+	return falling
+	
+	
+func detect_wall():
+	return (wall_detect.is_colliding() and wall_detect.get_collider().is_in_group('tilemap'))
 #	is_moving_left = player_location.x <= position.x 
 #
 #	if player_location.y > position.y:
@@ -166,7 +112,6 @@ func find_player():
 #
 #	if is_moving_left != before:   
 #		scale.x = -scale.x
-		
 
 func detect_ledge(_delta):
 	if not $edge.is_colliding() and is_on_floor():
@@ -208,3 +153,71 @@ func _on_playerHitter_body_entered(body):
 	emit_signal("dealDamage", damage)
 	$playerHitter.call_deferred('monitoring', false)
 	$playerCollision.call_deffered('monitoring', true)
+
+
+#func _physics_process(_delta):
+#	pass
+##	motion += applyGravity(_delta)
+##	move_to_target()
+#
+#func move_along_path(distance:float) -> void: 
+#	var starting_point = position
+#	for i in range(path.size()):
+#		var distance_to_next = starting_point.distance_to(path[0])
+#		if distance < distance_to_next and distance >= 0.0:
+#			position = starting_point.linear_interpolate(path[0], distance/distance_to_next)
+#		elif distance < 0.0: 
+#			position = path[0]
+#			var gravity = Vector2(0, GRAVITY)
+#			if not is_on_floor():
+#				var moment = move_and_slide(gravity, UP)
+#			set_process(false)
+#			break
+#		distance -= distance_to_next
+#		starting_point = path[0]
+#		path.remove(0)
+#
+#func set_path(value: PoolVector2Array) -> void:
+#	path = value
+#	if value.size() == 0:
+#		return
+#	set_process(true)
+
+#func move_to_target():
+#	var direction = global_position.direction_to(navi.get_next_location())
+#	motion = direction * MAXSPD
+#	motion = move_and_slide(motion, UP)
+#
+#func get_target_path(target_pos:Vector2):
+#	navi.set_target_location(target_pos)
+
+
+
+
+#func jump():
+#	motion.y = JUMPFORCE
+#
+#func jump_cut():
+#	if motion.y < -30:
+#		motion.y += 50
+#
+
+#func move_enemy(_delta):
+#
+#	motion.x += -ACCEL if is_moving_left else ACCEL
+#
+#	if abs(motion.x) > MAXSPD:
+#		motion.x = sign(motion.x) * MAXSPD
+#
+#	motion.y += GRAVITY if not is_on_floor() else 0
+#
+#	if motion.y > MAXFALLSPD:
+#		motion.y = MAXFALLSPD
+#
+#	motion = move_and_slide(motion, UP, true)
+#	$AnimatedSprite.play("walk")
+
+# returns true if player is close enough
+
+
+
